6836ce8ac35f015421f2b7306ea8059242ae5cce
Adriav <adriveravila64@gmail.com>
2026-01-19 13:45:02 -0600

    AllSetsScreen.kt name filter

--- app/src/main/java/com/adriav/tcgpokemon/models/AllSetsViewModel.kt
+++ app/src/main/java/com/adriav/tcgpokemon/models/AllSetsViewModel.kt
@@ -1,10 +1,16 @@
 package com.adriav.tcgpokemon.models
 
-import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
+import com.adriav.tcgpokemon.objects.normalize
 import dagger.hilt.android.lifecycle.HiltViewModel
 import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.SharingStarted
+import kotlinx.coroutines.flow.StateFlow
+import kotlinx.coroutines.flow.asStateFlow
+import kotlinx.coroutines.flow.combine
+import kotlinx.coroutines.flow.stateIn
 import kotlinx.coroutines.launch
 import net.tcgdex.sdk.TCGdex
 import net.tcgdex.sdk.models.SetResume
@@ -12,20 +18,42 @@ import javax.inject.Inject
 
 @HiltViewModel
 class AllSetsViewModel @Inject constructor(private val tcgdex: TCGdex) : ViewModel() {
-    private val _cardSets = MutableLiveData<Array<SetResume>>()
-    val cardSets = _cardSets
-    fun loadAllSets() {
-        viewModelScope.launch(Dispatchers.IO) {
-            try {
-                val response = tcgdex.fetchSets()
-                _cardSets.postValue(response!!)
-            } catch (e: Exception) {
-                e.printStackTrace()
+
+    private val _allSets = MutableStateFlow<Array<SetResume>>(emptyArray())
+    private val _searchQuery = MutableStateFlow("")
+    val searchQuery: StateFlow<String> = _searchQuery.asStateFlow()
+
+    val filteredSets: StateFlow<Array<SetResume>> =
+        combine(_allSets, _searchQuery) { sets, query ->
+            val normalizedQuery = query.normalize()
+
+            if (normalizedQuery.isBlank()) {
+                sets
+            } else {
+                sets
+                    .filter { set ->
+                        set.name.normalize().contains(normalizedQuery, ignoreCase = true)
+                    }
+                    .toTypedArray()
             }
+        }.stateIn(
+            scope = viewModelScope,
+            started = SharingStarted.WhileSubscribed(5_000),
+            initialValue = emptyArray()
+        )
+
+    init {
+        viewModelScope.launch(Dispatchers.IO) {
+            _allSets.value = tcgdex.fetchSets()!!
+            sortSets()
         }
     }
 
+    fun onSearchQueryChange(query: String) {
+        _searchQuery.value = query
+    }
+
     fun sortSets() {
-        _cardSets.value.sortWith(compareBy { it.name })
+        _allSets.value.sortWith(compareBy { it.name })
     }
 }
--- app/src/main/java/com/adriav/tcgpokemon/views/allview/AllSetsScreen.kt
+++ app/src/main/java/com/adriav/tcgpokemon/views/allview/AllSetsScreen.kt
@@ -1,31 +1,52 @@
 package com.adriav.tcgpokemon.views.allview
 
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Search
+import androidx.compose.material3.HorizontalDivider
+import androidx.compose.material3.Icon
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.OutlinedTextField
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
-import androidx.compose.runtime.livedata.observeAsState
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.font.FontStyle
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import com.adriav.tcgpokemon.R
 import com.adriav.tcgpokemon.models.AllSetsViewModel
-import com.adriav.tcgpokemon.objects.AppHeader
 import com.adriav.tcgpokemon.objects.CenteredProgressIndicator
 import com.adriav.tcgpokemon.views.items.SetItemView
 import net.tcgdex.sdk.models.SetResume
 
 @Composable
 fun AllSetsScreen(viewModel: AllSetsViewModel, navigateToSet: (String) -> Unit) {
-    val cardSets: Array<SetResume>? by viewModel.cardSets.observeAsState(null)
-    viewModel.loadAllSets()
+    val searchQuery by viewModel.searchQuery.collectAsState()
+    val sets by viewModel.filteredSets.collectAsState()
+    Column {
+        AllSetsHeader(searchQuery, viewModel)
+        if (sets.isEmpty() && searchQuery.isNotEmpty()) {
+            DisplaySearchError()
+        } else if (sets.isEmpty()) {
+            CenteredProgressIndicator()
+        } else {
+            SetsItems(sets, navigateToSet)
 
-    if (cardSets == null) {
-        CenteredProgressIndicator()
-    } else {
-        viewModel.sortSets()
-        Column {
-            AppHeader("All Sets")
-            SetsItems(cardSets!!, navigateToSet)
         }
     }
 }
@@ -42,6 +63,70 @@ fun SetsItems(cardSets: Array<SetResume>, navigateToSet: (String) -> Unit) {
             ) {
                 SetItemView(cardSets[index])
             }
+        }
+    }
+}
+
+@Composable
+fun AllSetsHeader(searchQuery: String, viewModel: AllSetsViewModel) {
+    Column {
+        Box(
+            modifier = Modifier
+                .fillMaxWidth()
+                .height(50.dp)
+
+        ) {
+            Text(
+                text = "All Sets",
+                fontSize = 28.sp,
+                fontStyle = FontStyle.Italic,
+                fontWeight = FontWeight.Bold,
+                textAlign = TextAlign.Center,
+                modifier = Modifier.fillMaxSize()
+            )
+        }
+        OutlinedTextField(
+            value = searchQuery,
+            onValueChange = viewModel::onSearchQueryChange,
+            modifier = Modifier
+                .fillMaxWidth()
+                .padding(horizontal = 8.dp)
+                .padding(bottom = 8.dp),
+            label = { Text("Search sets") },
+            singleLine = true,
+            leadingIcon = {
+                Icon(
+                    Icons.Default.Search,
+                    contentDescription = null,
+                    tint = MaterialTheme.colorScheme.primary
+                )
+            }
+        )
+    }
+    HorizontalDivider(Modifier.padding(bottom = 4.dp))
+}
+
+
+@Composable
+fun DisplaySearchError() {
+    Box(
+        modifier = Modifier.fillMaxSize(),
+        contentAlignment = Alignment.Center
+    ) {
+        Column(horizontalAlignment = Alignment.CenterHorizontally) {
+            Image(
+                painter = painterResource(R.drawable.verror_code_vector_icon),
+                contentDescription = null,
+                modifier = Modifier.size(300.dp)
+            )
+            Text(
+                modifier = Modifier.fillMaxWidth(),
+                text = "No Sets Found...",
+                color = MaterialTheme.colorScheme.error,
+                fontWeight = FontWeight.Bold,
+                fontSize = 28.sp,
+                textAlign = TextAlign.Center
+            )
         }
     }
 }
